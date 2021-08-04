44cf95dca92a38c672987471fc70d4d65a83c115
Ratchanon Sakdamanee <78460477+koala45601@users.noreply.github.com>
2021-08-05 03:38:20 +0700

    init commit

---
+++ envbox/school_web/school_app/urls.py
@@ -0,0 +1,8 @@
+#Url of app
+from school_app import views
+from django.urls import path
+from school_app.views import *
+
+urlpatterns = [
+    path('',views.homepage,name='home'),
+]
---
+++ envbox/school_web/school_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse,request
+
+# Create your views here.
+def homepage(request):
+    #return HttpResponse('<h1>Hello World</h1>')
+    return render (request,"Laout.html")
